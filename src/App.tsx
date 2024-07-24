3e956697ffe0de5770285f35c6eaf2ecd0e60987
rodrigo berbel <toteberbel@gmail.com>
2024-07-24 10:29:29 -0300

    added library

--- src/App.tsx
+++ src/App.tsx
@@ -2,66 +2,68 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "vanilla-cookieconsent/dist/cookieconsent.css";
+import * as CookieConsent from "vanilla-cookieconsent";
+import { useEffect } from "react";
 
 function App() {
-  // useEffect(() => {
-  //   CookieConsent.run({
-  //     categories: {
-  //       necessary: {
-  //         enabled: true, // this category is enabled by default
-  //         readOnly: true, // this category cannot be disabled
-  //       },
-  //       analytics: {},
-  //     },
+  useEffect(() => {
+    CookieConsent.run({
+      categories: {
+        necessary: {
+          enabled: true, // this category is enabled by default
+          readOnly: true, // this category cannot be disabled
+        },
+        analytics: {},
+      },
 
-  //     language: {
-  //       default: "en",
-  //       translations: {
-  //         en: {
-  //           consentModal: {
-  //             title: "We use cookies",
-  //             description: "Cookie modal description",
-  //             acceptAllBtn: "Accept all",
-  //             acceptNecessaryBtn: "Reject all",
-  //             showPreferencesBtn: "Manage Individual preferences",
-  //           },
-  //           preferencesModal: {
-  //             title: "Manage cookie preferences",
-  //             acceptAllBtn: "Accept all",
-  //             acceptNecessaryBtn: "Reject all",
-  //             savePreferencesBtn: "Accept current selection",
-  //             closeIconLabel: "Close modal",
-  //             sections: [
-  //               {
-  //                 title: "Somebody said ... cookies?",
-  //                 description: "I want one!",
-  //               },
-  //               {
-  //                 title: "Strictly Necessary cookies",
-  //                 description:
-  //                   "These cookies are essential for the proper functioning of the website and cannot be disabled.",
+      language: {
+        default: "en",
+        translations: {
+          en: {
+            consentModal: {
+              title: "We use cookies",
+              description: "Cookie modal description",
+              acceptAllBtn: "Accept all",
+              acceptNecessaryBtn: "Reject all",
+              showPreferencesBtn: "Manage Individual preferences",
+            },
+            preferencesModal: {
+              title: "Manage cookie preferences",
+              acceptAllBtn: "Accept all",
+              acceptNecessaryBtn: "Reject all",
+              savePreferencesBtn: "Accept current selection",
+              closeIconLabel: "Close modal",
+              sections: [
+                {
+                  title: "Somebody said ... cookies?",
+                  description: "I want one!",
+                },
+                {
+                  title: "Strictly Necessary cookies",
+                  description:
+                    "These cookies are essential for the proper functioning of the website and cannot be disabled.",
 
-  //                 //this field will generate a toggle linked to the 'necessary' category
-  //                 linkedCategory: "necessary",
-  //               },
-  //               {
-  //                 title: "Performance and Analytics",
-  //                 description:
-  //                   "These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.",
-  //                 linkedCategory: "analytics",
-  //               },
-  //               {
-  //                 title: "More information",
-  //                 description:
-  //                   'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>',
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //     },
-  //   });
-  // }, []);
+                  //this field will generate a toggle linked to the 'necessary' category
+                  linkedCategory: "necessary",
+                },
+                {
+                  title: "Performance and Analytics",
+                  description:
+                    "These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.",
+                  linkedCategory: "analytics",
+                },
+                {
+                  title: "More information",
+                  description:
+                    'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  }, []);
 
   return (
     <>
