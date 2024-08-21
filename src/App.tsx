382aa5ea0548b45b3d8da8603fe61392cf3bc579
rodrigo berbel <toteberbel@gmail.com>
2024-08-21 20:58:22 -0300

    added latest commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,135 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "vanilla-cookieconsent/dist/cookieconsent.css";
-import * as CookieConsent from "vanilla-cookieconsent";
-import { useEffect } from "react";
+import "rodrigo-berbel-cookieconsent/dist/cookieconsent.css";
+import * as CookieConsent from "rodrigo-berbel-cookieconsent";
+
+CookieConsent.run({
+  disablePageInteraction: true,
+
+  cookie: {
+    name: "cc_cookie_demo2",
+  },
+
+  guiOptions: {
+    consentModal: {
+      layout: "box wide",
+      position: "bottom right",
+    },
+    preferencesModal: {
+      layout: "box",
+    },
+  },
+
+  onFirstConsent: () => {
+    console.log("onFirstAction fired");
+  },
+
+  onConsent: () => {
+    console.log("onConsent fired ...");
+  },
+
+  onChange: () => {
+    console.log("onChange fired ...");
+  },
+
+  categories: {
+    necessary: {
+      readOnly: true,
+      enabled: true,
+    },
+    analytics: {
+      autoClear: {
+        cookies: [
+          {
+            name: /^(_ga|_gid)/,
+          },
+        ],
+      },
+    },
+    ads: {},
+  },
+
+  language: {
+    default: "en",
+
+    translations: {
+      en: {
+        consentModal: {
+          title: "Hello traveller, it's cookie time!",
+          description:
+            "Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent.",
+          acceptAllBtn: "Accept",
+          acceptNecessaryBtn: "Reject",
+          showPreferencesBtn: "Manage with BTS",
+          closeIconLabel: "Reject all and close",
+          footer: `
+                        <a href="#link">Privacy Policy</a>
+                        <a href="#link">Impressum</a>
+                    `,
+        },
+        preferencesModal: {
+          title: "Cookie preferences",
+          acceptAllBtn: "Accept all",
+          acceptNecessaryBtn: "Reject all",
+          savePreferencesBtn: "Save preferences",
+          sections: [
+            {
+              title: "Cookie usage",
+              description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. For more details, refer to our <a href="#" class="cc__link">privacy policy</a>.',
+            },
+            {
+              title: "Strictly necessary cookies",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              linkedCategory: "necessary",
+            },
+            {
+              title: "Performance and analytics cookies",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              linkedCategory: "analytics",
+              cookieTable: {
+                caption: "Cookie table",
+                headers: {
+                  name: "Cookie",
+                  domain: "Domain",
+                  desc: "Description",
+                },
+                body: [
+                  {
+                    name: "_ga",
+                    domain: "yourdomain.com",
+                    desc: "description ...",
+                  },
+                  {
+                    name: "_gid",
+                    domain: "yourdomain.com",
+                    desc: "description ...",
+                  },
+                ],
+              },
+            },
+            {
+              title: "Advertisement and targeting cookies",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              linkedCategory: "ads",
+            },
+            {
+              title: "More information",
+              description:
+                'For any queries in relation to our policy on cookies and your choices, please <a class="cc__link" href="#yourdomain.com">contact me</a>.',
+            },
+          ],
+        },
+      },
+    },
+  },
+});
 
 function App() {
-  useEffect(() => {
-    CookieConsent.run({
-      categories: {
-        necessary: {
-          enabled: true, // this category is enabled by default
-          readOnly: true, // this category cannot be disabled
-        },
-        analytics: {},
-      },
-
-      language: {
-        default: "en",
-        translations: {
-          en: {
-            consentModal: {
-              title: "We use cookies",
-              description: "Cookie modal description",
-              acceptAllBtn: "Accept all",
-              acceptNecessaryBtn: "Reject all",
-              showPreferencesBtn: "Manage Individual preferences",
-            },
-            preferencesModal: {
-              title: "Manage cookie preferences",
-              acceptAllBtn: "Accept all",
-              acceptNecessaryBtn: "Reject all",
-              savePreferencesBtn: "Accept current selection",
-              closeIconLabel: "Close modal",
-              sections: [
-                {
-                  title: "Somebody said ... cookies?",
-                  description: "I want one!",
-                },
-                {
-                  title: "Strictly Necessary cookies",
-                  description:
-                    "These cookies are essential for the proper functioning of the website and cannot be disabled.",
-
-                  //this field will generate a toggle linked to the 'necessary' category
-                  linkedCategory: "necessary",
-                },
-                {
-                  title: "Performance and Analytics",
-                  description:
-                    "These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.",
-                  linkedCategory: "analytics",
-                },
-                {
-                  title: "More information",
-                  description:
-                    'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>',
-                },
-              ],
-            },
-          },
-        },
-      },
-    });
-  }, []);
-
   return (
     <>
       <div>
